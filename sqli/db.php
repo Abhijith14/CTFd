465e682bc99565bd9d1bbed2ba3c87e61565b569
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2022-06-12 10:40:11 +0530

    Update db.php

--- sqli/db.php
+++ sqli/db.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$host = "ec2-52-4-111-46.compute-1.amazonaws.com";
-$user = "zkhhstixqwqjux";
-$pass = "c6e8a0206b25dd727fc7bcc564be9c221c4537e02caf6d7ebb7478cd385fd3c5";
-$database = "d1nsnpbdjf51k4";
-$port = "5432";
+$host = "";
+$user = "";
+$pass = "";
+$database = "";
+$port = "";
 
 try{
 
